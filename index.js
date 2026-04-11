0d0644c9899ff27c39fcd6b25d239561d54ef205
Harsh <harshpatle004@gmail.com>
2026-04-11 09:47:18 +0530

    added profile

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
 const express = require("express");
 const connectDB = require("./databse/db");
-const app = express()
-const dotenv = require("dotenv").config()
-const authRoutes = require("./routes/auth")
+const app = express();
+const dotenv = require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const userRoute = require("./routes/userRoute");
+const cors = require("cors");
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoute);
 
-app.listen(5000,()=>{
-    connectDB()
-    console.log("app is running")
-})  
-
+app.listen(5000, () => {
+  connectDB();
+  console.log("app is running on port 5000");
+});
